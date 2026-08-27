108d7edb967cacdd4b47e63b9f02c5fc1da24783
jlashmet <jlashmet@gmail.com>
2026-08-27 16:55:35 -0700

    Wire events runtime

---
+++ ticketmaster/events/bootstrap/src/main/java/com/systemdesign/ticketmaster/events/bootstrap/EventsServiceApplication.java
@@ -0,0 +1,35 @@
+package com.systemdesign.ticketmaster.events.bootstrap;
+
+import com.systemdesign.ticketmaster.events.application.GetEventHandler;
+import com.systemdesign.ticketmaster.events.domain.EventRepository;
+import com.systemdesign.ticketmaster.events.infrastructure.output.DynamoEventRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+@SpringBootApplication(scanBasePackages = "com.systemdesign.ticketmaster.events.infrastructure.input")
+public class EventsServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventsServiceApplication.class, args);
+    }
+
+    @Bean
+    DynamoDbClient dynamoDbClient(@Value("${ticketmaster.aws.region:us-west-2}") String region) {
+        return DynamoDbClient.builder().region(Region.of(region)).build();
+    }
+
+    @Bean
+    EventRepository eventRepository(
+            DynamoDbClient dynamoDbClient,
+            @Value("${ticketmaster.events.table-name:ticketmaster-events}") String tableName) {
+        return new DynamoEventRepository(dynamoDbClient, tableName);
+    }
+
+    @Bean
+    GetEventHandler getEventHandler(EventRepository eventRepository) {
+        return new GetEventHandler(eventRepository);
+    }
+}

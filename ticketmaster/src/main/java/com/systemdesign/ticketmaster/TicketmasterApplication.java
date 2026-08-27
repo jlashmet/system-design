2f9990220c0c51420000257bac22021a6de8be1f
jlashmet <jlashmet@gmail.com>
2026-08-27 11:00:55 -0700

    Add Spring Boot application

---
+++ ticketmaster/src/main/java/com/systemdesign/ticketmaster/TicketmasterApplication.java
@@ -0,0 +1,12 @@
+package com.systemdesign.ticketmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketmasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketmasterApplication.class, args);
+    }
+}

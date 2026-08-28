2d13237fc1b1bfcb07f6af96e60d767f78b5d8e5
jlashmet <jlashmet@gmail.com>
2026-08-28 11:00:14 -0700

    Remove output-specific search exception

--- ticketmaster/search/infrastructure/output/src/main/java/com/systemdesign/ticketmaster/search/infrastructure/output/OpenSearchQueryException.java
+++
@@ -1,7 +0,0 @@
-package com.systemdesign.ticketmaster.search.infrastructure.output;
-
-public final class OpenSearchQueryException extends RuntimeException {
-    public OpenSearchQueryException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}

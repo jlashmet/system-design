c1bb809bcca47d1d0e4895460a9ad00fadb32f37
jlashmet <jlashmet@gmail.com>
2026-08-27 18:09:12 -0700

    Add OpenSearch indexing exception

---
+++ ticketmaster/search/infrastructure/output/src/main/java/com/systemdesign/ticketmaster/search/infrastructure/output/OpenSearchIndexException.java
@@ -0,0 +1,7 @@
+package com.systemdesign.ticketmaster.search.infrastructure.output;
+
+public final class OpenSearchIndexException extends RuntimeException {
+    public OpenSearchIndexException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}

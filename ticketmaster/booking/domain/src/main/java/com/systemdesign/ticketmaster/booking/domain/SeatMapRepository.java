5be5e9ad9ea1afd062edd704a1ef3a48ff41f7cc
jlashmet <jlashmet@gmail.com>
2026-08-28 10:52:31 -0700

    Add seat map section discovery contract

--- ticketmaster/booking/domain/src/main/java/com/systemdesign/ticketmaster/booking/domain/SeatMapRepository.java
+++ ticketmaster/booking/domain/src/main/java/com/systemdesign/ticketmaster/booking/domain/SeatMapRepository.java
@@ -4,5 +4,6 @@ import java.util.List;
 
 public interface SeatMapRepository {
     void upsert(SeatMapSeat seat);
+    List<SectionId> findSections(EventId eventId);
     List<SeatMapSeat> findSection(EventId eventId, SectionId sectionId);
 }

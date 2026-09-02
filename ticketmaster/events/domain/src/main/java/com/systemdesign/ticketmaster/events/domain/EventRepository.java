5b09b7c741c05daa794fbc14f995b615948b426e
jlashmet <jlashmet@gmail.com>
2026-09-02 10:41:05 -0700

    Add event create repository contract

--- ticketmaster/events/domain/src/main/java/com/systemdesign/ticketmaster/events/domain/EventRepository.java
+++ ticketmaster/events/domain/src/main/java/com/systemdesign/ticketmaster/events/domain/EventRepository.java
@@ -3,5 +3,7 @@ package com.systemdesign.ticketmaster.events.domain;
 import java.util.Optional;
 
 public interface EventRepository {
+    void create(Event event);
+
     Optional<Event> findById(EventId eventId);
 }

2e19b3d47766b7440eee6d15ec7041cb58dcc9fb
jlashmet <jlashmet@gmail.com>
2026-09-02 10:43:41 -0700

    Keep event repository read-only

--- ticketmaster/events/domain/src/main/java/com/systemdesign/ticketmaster/events/domain/EventRepository.java
+++ ticketmaster/events/domain/src/main/java/com/systemdesign/ticketmaster/events/domain/EventRepository.java
@@ -3,7 +3,5 @@ package com.systemdesign.ticketmaster.events.domain;
 import java.util.Optional;
 
 public interface EventRepository {
-    void create(Event event);
-
     Optional<Event> findById(EventId eventId);
 }
